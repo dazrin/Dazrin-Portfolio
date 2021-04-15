49dce398b3411d26400d6753a0b3d4ae30ed0619
Dazrin Tioseco <xdazee@gmail.com>
2021-04-15 14:22:59 -0700

    Created Routes & set up sanity.io

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+ import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Home from './components/Home';
+import About from './components/About';
+import SinglePost from './components/SinglePost';
+import Post from './components/Post';
+import Project from './components/Project';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+    <Switch>
+      <Route component={Home} path='/' exact />
+      <Route component={About} path='/About' />
+      <Route component={SinglePost} path='/Post/:slug' />
+      <Route component={Post} path='/Post' />
+      <Route component={Project} path='/Project' />
+  </Switch>
+  </BrowserRouter>
   );
 }
 
