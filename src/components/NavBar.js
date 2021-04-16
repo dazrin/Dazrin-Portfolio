a34dd5e8df04b3a08d50dff05a7613749fcadd09
Dazrin Tioseco <xdazee@gmail.com>
2021-04-16 13:21:59 -0700

    Added social icons. Added bg highlight to active navlinks

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,11 +1,12 @@
 import React from "react"
 import { NavLink } from "react-router-dom";
+import { SocialIcon } from "react-social-icons";
 
 
 export default function NavBar() {
     return (
         <header className="bg-purple-900">
-            <div className="container mx-auto flex justify-between">
+            <div className="container mx-auto py-4 flex justify-between">
                 <nav>
                     <NavLink to="/"
                      exact
@@ -14,18 +15,26 @@ export default function NavBar() {
                         Dazrin
                     </NavLink>
                     <NavLink to="/Post"
-                    className="inline-flex items-center py3 px-3 my-6 rounded text-blue-50 hover:text-blue-300">
+                        className="inline-flex items-center py3 px-3 my-6 rounded text-blue-50 hover:text-blue-300"
+                        activeClassName="text-blue-100 bg-blue-500">
                         Blog Posts
                     </NavLink>
                     <NavLink to="/Project"
-                    className="inline-flex items-center py3 px-3 my-6 rounded text-blue-50 hover:text-blue-300">
+                    className="inline-flex items-center py3 px-3 my-6 rounded text-blue-50 hover:text-blue-300"
+                    activeClassName="text-blue-100 bg-blue-500">
                         Projects
                     </NavLink>
                     <NavLink to="/About"
-                    className="inline-flex items-center py3 px-3 my-6 rounded text-blue-50 hover:text-blue-300">
+                    className="inline-flex items-center py3 px-3 my-6 rounded text-blue-50 hover:text-blue-300"
+                    activeClassName="text-blue-100 bg-blue-500">
                         About Me
                     </NavLink>
                 </nav>
+                <div className="inline-flex py-3 px-3 my-1">
+                    <SocialIcon url="https://twitter.com/dazrinDev" className="mr-4" target="_blank" fgColor="#fff" style={{ height: 35, width: 35 }} />
+                    <SocialIcon url="https://github.com/dazrin" className="mr-4" target="_blank" fgColor="#fff" style={{ height: 35, width: 35 }} />
+                    <SocialIcon url="https://www.linkedin.com/in/dazrin-tioseco/" className="mr-4" target="_blank" fgColor="#fff" style={{ height: 35, width: 35 }} />
+                </div>
             </div>
         </header>
     )
