6d1c12b4bc2e8277d3e0280f710983a6ed41aa3f
Dazrin Tioseco <xdazee@gmail.com>
2021-04-23 16:10:00 -0700

    Got blog post and project pages working with sanity io

--- src/components/SinglePost.js
+++ src/components/SinglePost.js
@@ -1,5 +1,53 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
+import { useParams } from "react-router-dom";
+import sanityClient from "../client.js";
+import imageUrlBuilder from "@sanity/image-url";
+
+const builder = imageUrlBuilder(sanityClient);
+function urlFor(source) {
+    return builder.image(source)
+}
 
 export default function SinglePost() {
-    return <h1>SinglePost Page!</h1>
+    const [ singlePost, setSinglePost ] = useState(null);
+    const { slug } = useParams();
+
+    useEffect(() => {
+        sanityClient.fetch(`*[slug.current == "${slug}"]{
+            title,
+            _id,
+            slug,
+            mainImage{
+                asset->{
+                    _id,
+                    url
+                }
+            },
+            body,
+            "name": author->name,
+            "authorImage": author->image
+        }`).then((data) => setSinglePost(data[0]))
+        .catch(console.error);
+    
+    }, [slug]);
+
+    if (!singlePost) return <div> Loading... </div>;
+
+    return (
+        <main>
+            <article>
+                <header>
+                    <div>
+                        <div>
+                            <h1>
+                                <div>
+                                    
+                                </div>
+                            </h1>
+                        </div>
+                    </div>
+                </header>
+            </article>
+        </main>
+    )
 }
