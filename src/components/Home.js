22b5af39ef099bf8dc645eca7b2727f079c68e79
Dazrin Tioseco <xdazee@gmail.com>
2021-04-26 14:57:22 -0700

    Attempting to add profile image on home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,30 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
+import sanityClient from "../client.js";
 import image from "../background.jpg";
+import imageUrlBuilder from "@sanity/image-url";
+
+const builder = imageUrlBuilder(sanityClient);
+function urlFor(source) {
+    return builder.image(source)
+}
 
 export default function Home() {
+    const [author, setAuthor] = useState(null);
+
+    useEffect(() => {
+        sanityClient.fetch(`
+          *[_type == "author"]{
+            name,
+            bio,
+            "authorImage": image.asset->url
+        }`
+        )
+        .then((data) => setAuthor(data[0]))
+        .catch(console.error);
+    }, []);
+
+    if (!author) return <div>Loading...</div>;
+
     return (
         <main>
             <img 
@@ -9,9 +32,19 @@ export default function Home() {
             alt="universe background" 
             className="absolute object-cover w-full h-full"
             />
-            <section className="relative flex justify-center min-h-screen pt-12 lg:pt-64 px-8">
-                <h1 className="text-4xl text-pink-700 font-bold cursive leading-none lg:leading-snug home-name">Hello! I'm Dazrin.</h1>
+            <section className="relative grid justify-center min-h-screen pt-12 lg:pt-64 px-8">
+                <h1 className="text-4xl text-pink-400 font-bold cursive leading-none lg:leading-snug home-name">Hello! I'm <span className="text-yellow-200">Dazrin.</span></h1>
+
+
             </section>
+            
         </main>
     )
 }
+/* User Image
+                  <img
+                      src={urlFor(author.authorImage).url()}
+                      className="mx-auto rounded w-32 h-32 lg:w-64 lg:h-64 "
+                      alt={author.name}
+                  />
+*/
