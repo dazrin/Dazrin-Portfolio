f759affd90d07feea4e5b3ce655f67c30970568d
Dazrin Tioseco <xdazee@gmail.com>
2021-04-26 13:13:04 -0700

    Finished singlePost component

--- src/components/SinglePost.js
+++ src/components/SinglePost.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react"
 import { useParams } from "react-router-dom";
 import sanityClient from "../client.js";
 import imageUrlBuilder from "@sanity/image-url";
+import BlockContent from "@sanity/block-content-to-react";
 
 const builder = imageUrlBuilder(sanityClient);
 function urlFor(source) {
@@ -34,19 +35,41 @@ export default function SinglePost() {
     if (!singlePost) return <div> Loading... </div>;
 
     return (
-        <main>
-            <article>
-                <header>
-                    <div>
-                        <div>
-                            <h1>
-                                <div>
-                                    
-                                </div>
+        <main className="bg-gray-200 min-h-screen p-12">
+            <article className="container shadow-lg mx-auto bg-blue-100 rounded-lg">
+                <header className="relative">
+                    <div className="absolute h-full w-full flex item-center justify-center p-8">
+                        <div className="bg-white bg-opacity-75 rounded p-12">
+                            <h1 className="cursive text-3xl lg:text-6xl mb-4">
+                                {singlePost.title}
                             </h1>
+                            <div className="flex justify-center text-gray-800">
+                                <img
+                                src={urlFor(singlePost.authorImage).url()}
+                                alt={singlePost.name}
+                                className="w-10 h-10 rounded-full"
+                                />
+                                <p className="cursive flex items-center pl-2 text-2xl">
+                                    {singlePost.name}
+                                </p>
+                            </div>
                         </div>
                     </div>
+                    <img
+                    src={singlePost.mainImage.asset.url}
+                    alt={singlePost.title}
+                    className="w-full object-cover rounded-t"
+                    style={{ height: "400px" }}
+                    />
                 </header>
+                <div
+                className="px-16 lg:px-48 py-12 lg:py-20 prose lg:prose-xl max-w-full">
+                    <BlockContent
+                      blocks={singlePost.body}
+                      projectId="olfmsbp0"
+                      dataset="production"
+                    />
+                    </div>
             </article>
         </main>
     )
