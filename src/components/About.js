1584d3a6d7ae59c8d55b1f1bbedd90714e813b20
Dazrin Tioseco <xdazee@gmail.com>
2021-04-28 21:34:55 -0700

    Changed styling on about page

--- src/components/About.js
+++ src/components/About.js
@@ -6,7 +6,7 @@ import BlockContent from "@sanity/block-content-to-react";
 
 const builder = imageUrlBuilder(sanityClient);
 function urlFor(source) {
-    return builder.image(source)
+  return builder.image(source)
 }
 
 export default function About() {
@@ -34,7 +34,7 @@ export default function About() {
               className="absolute w-full"
             />
             <div className="p-10 lg:pt-48 container mx-auto relative">
-                <section className="bg-pink-800 rounded-lg shadow-2xl lg:flex p-20">
+                <section className="bg-blue-900 bg-opacity-90 rounded-lg shadow-2xl lg:flex p-20">
                     <img
                       src={urlFor(author.authorImage).url()}
                       className="rounded w-32 h-32 lg:w-64 lg:h-64 mr-8"
@@ -43,7 +43,7 @@ export default function About() {
                     <div
                       className="text-lg flex flex-col justify-center"
                     >
-                        <h1 className="cursive text-6xl text-pink-300 mb-4">
+                        <h1 className="cursive text-6xl text-purple-300 mb-4">
                             Hey there. I'm{" "}
                             <span className="text-yellow-100">
                               {author.name}
